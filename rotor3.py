1b1ac076f8852712061d9726995a8479165be8ee
Ruichen Zheng <ankbzpx@hotmail.com>
2024-06-26 21:04:12 +0800

    better abstraction

--- rotor3.py
+++ rotor3.py
@@ -30,17 +30,6 @@ class G3:
     def tree_unflatten(cls, aux_data, children):
         return cls(*children)
 
-    @classmethod
-    def from_pairs(cls, a: np.ndarray, b: np.ndarray):
-        a = a / np.linalg.norm(a)
-        b = b / np.linalg.norm(b)
-        c = np.dot(a, b)
-        c_01 = a[0] * b[1] - a[1] * b[0]
-        c_12 = a[1] * b[2] - a[2] * b[1]
-        c_20 = a[2] * b[0] - a[0] * b[2]
-
-        return cls(c, 0, 0, 0, c_01, c_12, c_20, 0)
-
     @staticmethod
     def geometric_product(u, v):
         C = np.outer(jax.tree.leaves(v), jax.tree.leaves(u))
@@ -63,21 +52,6 @@ class G3:
             3, 4] + C[4, 3]
 
         return G3(c, c_0, c_1, c_2, c_01, c_12, c_20, c_012)
-
-    @property
-    def dag(self) -> Self:
-        return self.conjugate()
-
-    def conjugate(self) -> Self:
-        return G3(self.c, 0, 0, 0, -self.c_01, -self.c_12, -self.c_20, 0)
-
-    def rotate(self, a):
-        # R a R^\dag
-        a = G3(0, a[0], a[1], a[2], 0, 0, 0, 0)
-        a_rot = R * a * R.dag
-        ic(a_rot)
-        assert np.isclose(a_rot.c_012, 0)
-        return np.array([a_rot.c_0, a_rot.c_1, a_rot.c_2])
 
     def __add__(self, other) -> Self:
         if isinstance(other, G3):
@@ -108,6 +82,166 @@ class G3:
         return -1 * self
 
 
+@register_pytree_node_class
+@define
+class Bivector3:
+    c_01: float
+    c_12: float
+    c_20: float
+
+    def tree_flatten(self):
+        children = (self.c_01, self.c_12, self.c_20)
+        aux_data = None
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(*children)
+
+    @classmethod
+    def from_wedge(cls, a: np.ndarray, b: np.ndarray):
+        c_01 = a[0] * b[1] - a[1] * b[0]
+        c_12 = a[1] * b[2] - a[2] * b[1]
+        c_20 = a[2] * b[0] - a[0] * b[2]
+
+        return cls(c_01, c_12, c_20)
+
+    @staticmethod
+    def bivector_bivector_product(B_a, B_b) -> Self:
+        B_out = B_a.to_G3() * B_b.to_G3()
+        return Rotor3(B_out.c, Bivector3(B_out.c_01, B_out.c_12, B_out.c_20))
+
+    @property
+    def squared_norm(self) -> float:
+        return np.square(jax.tree.leaves(self)).sum()
+
+    @property
+    def norm(self) -> float:
+        return np.sqrt(self.squared_norm)
+
+    def normalize(self) -> Self:
+        norm = self.norm + 1e-8
+        return tree_map(lambda x: x / norm, self)
+
+    def to_G3(self):
+        return G3(0, 0, 0, 0, self.c_01, self.c_12, self.c_20, 0)
+
+    def inverse(self) -> Self:
+        squared_norm = self.squared_norm + 1e-8
+        return tree_map(lambda x: x / squared_norm, self)
+
+    def __add__(self, other) -> Self:
+        if isinstance(other, Bivector3):
+            return tree_map(lambda x, y: x + y, self, other)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other) -> Self:
+        if isinstance(other, (float, int)):
+            return tree_map(lambda x: x * other, self)
+        elif isinstance(other, Bivector3):
+            return Bivector3.bivector_bivector_product(self, other)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other) -> Self:
+        if isinstance(other, (float, int)):
+            return self * other
+        elif isinstance(other, Bivector3):
+            return Bivector3.bivector_bivector_product(other, self)
+        else:
+            return NotImplemented
+
+    def __truediv__(self, other):
+        return (1 / other) * self
+
+    def __neg__(self):
+        return -1 * self
+
+
+@register_pytree_node_class
+@define
+class Rotor3:
+    c: float
+    B: Bivector3
+
+    def tree_flatten(self):
+        B_flatten, _ = self.B.tree_flatten()
+        children = (self.c, B_flatten)
+        aux_data = None
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        c, B_flatten = children
+        return cls(c, Bivector3.tree_unflatten(None, B_flatten))
+
+    @classmethod
+    def from_pairs(cls, a: np.ndarray, b: np.ndarray, normalize=True):
+        c = np.dot(a, b)
+        B = Bivector3.from_wedge(a, b)
+        if normalize:
+            norm = np.linalg.norm(a) * np.linalg.norm(b)
+            c /= norm
+            B /= norm
+
+        return cls(c, B)
+
+    @property
+    def dag(self) -> Self:
+        return self.conjugate()
+
+    @staticmethod
+    def rotor_rotor_product(R_a, R_b) -> Self:
+        R_out = R_a.to_G3() * R_b.to_G3()
+        return Rotor3(R_out.c, Bivector3(R_out.c_01, R_out.c_12, R_out.c_20))
+
+    def to_G3(self):
+        return G3(self.c, 0, 0, 0, self.B.c_01, self.B.c_12, self.B.c_20, 0)
+
+    def conjugate(self) -> Self:
+        return Rotor3(self.c, -1 * self.B)
+
+    def __add__(self, other) -> Self:
+        if isinstance(other, Rotor3):
+            return tree_map(lambda x, y: x + y, self, other)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other) -> Self:
+        if isinstance(other, Rotor3):
+            return Rotor3.rotor_rotor_product(self, other)
+        elif isinstance(other, (float, int)):
+            return tree_map(lambda x: x * other, self)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other) -> Self:
+        if isinstance(other, Rotor3):
+            return Rotor3.rotor_rotor_product(other, self)
+        elif isinstance(other, (float, int)):
+            return tree_map(lambda x: x * other, self)
+        else:
+            return NotImplemented
+
+    def __truediv__(self, other):
+        return (1 / other) * self
+
+    def __neg__(self):
+        return -1 * self
+
+    def rotate(self, a):
+        R_g3 = G3(R.c, 0, 0, 0, R.B.c_01, R.B.c_12, R.B.c_20, 0)
+        a_g3 = G3(0, a[0], a[1], a[2], 0, 0, 0, 0)
+        R_dag_g3 = G3(R.dag.c, 0, 0, 0, R.dag.B.c_01, R.dag.B.c_12,
+                      R.dag.B.c_20, 0)
+        b_g3 = R_g3 * a_g3 * R_dag_g3
+
+        assert np.isclose(b_g3.c_012, 0)
+
+        return np.array([b_g3.c_0, b_g3.c_1, b_g3.c_2])
+
+
 def angle_between(a, b):
     theta = np.arccos(np.dot(a, b) / np.linalg.norm(a) / np.linalg.norm(b))
     return np.rad2deg(theta)
@@ -121,7 +255,7 @@ if __name__ == '__main__':
 
     ic(angle_between(a, b))
 
-    R = G3.from_pairs(a, b)
+    R = Rotor3.from_pairs(a, b)
     v2 = R.rotate(v)
 
     ic(angle_between(v, v2))
